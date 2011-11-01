b58bfc56457acea1f5dac3d5f4562b3538785a5c
demosthenes2k8 <devnull@localhost>
2011-10-31 21:10:49 -0400

    Fixed another 32/64 bit error, thanks to Ben

--- src/errors.d
+++ src/errors.d
@@ -16,7 +16,7 @@ class OratrParseException : OratrBaseException {
 }
 
 class OratrInvalidArgumentException : OratrBaseException {
-	this( string type, int position, string file = __FILE__, size_t line = __LINE__ )
+	this( string type, size_t position, string file = __FILE__, size_t line = __LINE__ )
     {
         super( "Invalid argument type \""~type~"\" in position "~to!string(position), file, line );
     }
